4cdc3a161de2a96f0082324f05f739eee1d986da
liuliang <liu.liang@shuweikeji.com>
2018-12-27 14:08:41 +0800

    1

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading;
 using Tibos.Test;
@@ -9,8 +10,7 @@ namespace Test
 
     class Model
     {
-        public int Height { get; set; }
-        public int Width { get; set; }
+
     }
 
     class Program
@@ -18,46 +18,20 @@ namespace Test
 
         static void Main(string[] args)
         {
-            List<Model> list = new List<Model>();
-            List<List<Model>> res = new List<List<Model>>();
-            //初始化数据
-            for (int i = 0; i < 10000; i++)
-            {
-                var heigth = new Random().Next(1100, 1250);
-                var width = new Random().Next(1200, 1300);
-                //对数据进行整理 width>= height
-                if(heigth > width)
-                {
-                    list.Add(new Model() { Height = width, Width = heigth });
-                }
-                else
-                {
-                    list.Add(new Model() { Height = heigth, Width = width });
-                }
-            }
-            //以最小的width为基准
-            while (list.Count>0)
-            {
-                var min_width_model = list.OrderBy(m => m.Width).First();
-                var min_list = list.Where(m => m.Width <= min_width_model.Width + 3).ToList();
-                //对height拆分
-                while(min_list.Count > 0)
-                {
-                    var min_height_model = min_list.OrderBy(m => m.Height).First();
-                    var height_min_list = min_list.Where(m => m.Height <= min_height_model.Height + 3).ToList();
-                    res.Add(height_min_list);
-                    min_list.RemoveAll(m => height_min_list.Contains(m));
-                    list.RemoveAll(m => height_min_list.Contains(m));
-                }
-            }
+            string SavePath = @"F:\ExcelTemp\whitelist_sample.xlsx";
+            Console.WriteLine("ETH地址             此日期前不可购买             此日期前不可售出             KYC/ AML有效期");
 
-            for (int i = 0; i < res.Count; i++)
+            DataTable dt = OfficeHelper.ReadExcelToDataTable(SavePath);
+            for (int i = 0; i < dt.Rows.Count; i++)
             {
-                Console.WriteLine($"第{i}组数据,共{res[i].Count}条");
-                foreach (var item in res[i])
+                Console.WriteLine($"第{i}行数据:");
+                for (int j = 0; j < dt.Columns.Count; j++)
                 {
-                    Console.WriteLine($"-->Height:{item.Height},Width:{item.Width}");
+
+                    Console.Write(dt.Rows[i][j].ToString() + "             ");
+
                 }
+                Console.WriteLine("");
             }
             Console.Read();
         }
